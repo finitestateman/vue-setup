3b9f6c511cf22f763600f169dc67255a43112113
username <user@email.com>
2025-03-19 23:55:01 +0900

    PanelMenu + Splitter

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,89 @@ import Button from 'primevue/button';
 import { ref } from 'vue';
 import type { MenuItem } from 'primevue/menuitem';
 import Menubar from 'primevue/menubar';
+import PanelMenu from 'primevue/panelmenu';
 import Toast from 'primevue/toast';
 import { useToast } from 'primevue';
+import Splitter from 'primevue/splitter';
+import SplitterPanel from 'primevue/splitterpanel';
 
 const text = ref<string>('')
 const toast = useToast()
 
-const menuItems = ref<MenuItem[]>([
+const panelItems = ref<MenuItem[]>([
+  {
+    key: 'zelda',
+    icon: 'pi pi-image',
+    label: 'The Legend of Zelda',
+    items: [
+      {
+        icon: 'pi pi-image',
+        label: 'Breath of the Wild',
+      },
+      {
+        icon: 'pi pi-plus',
+        url: 'https://www.nintendo.com/us/store/products/super-mario-odyssey-switch/',
+        target: '_blank',
+        label: 'Tears of the Kingdom',
+      },
+      {
+        icon: 'pi pi-file',
+        label: 'Echoes of Wisdom',
+      }
+    ]
+  },
+  {
+    key: 'mario',
+    icon: 'pi pi-sync',
+    label: 'Super Mario',
+    items: [
+      {
+        icon: 'pi pi-print',
+        label: 'Super Mario World',
+      },
+      {
+        icon: 'pi pi-plus',
+        url: 'https://www.nintendo.com/us/store/products/super-mario-odyssey-switch/',
+        target: '_blank',
+        label: 'Super Mario Odyssey',
+      },
+      {
+        icon: 'pi pi-file',
+        label: 'Mario Kart 8 Deluxe',
+      }
+    ]
+  },
+  {
+    key: 'pokemon',
+    icon: 'pi pi-phone',
+    label: 'Pokémon',
+    items: [
+      {
+        icon: 'pi pi-print',
+        label: 'Super Mario World',
+      },
+      {
+        icon: 'pi pi-plus',
+        url: 'https://www.nintendo.com/us/store/products/super-mario-odyssey-switch/',
+        target: '_blank',
+        label: 'Super Mario Odyssey',
+      },
+      {
+        icon: 'pi pi-file',
+        label: 'Mario Kart 8 Deluxe',
+      }
+    ]
+  },
+  {
+    key: 'splatoon',
+    icon: 'pi pi-mobile',
+    label: 'Splatoon',
+    command: () => toast.add({ severity: 'error', summary: 'Not Implemented', detail: 'Splatoon is not implemented yet', life: 3000 }),
+  }
+]);
+
+
+const menubarItems = ref<MenuItem[]>([
   {
     key: 'zelda',
     icon: 'pi pi-home',
@@ -57,16 +133,29 @@ const menuItems = ref<MenuItem[]>([
 
 <template>
   <Toast />
-  <Menubar :model="menuItems">
-    <template #end>
-        <InputText id="game_search" v-model="text" placeholder="Search" />
-        <Button label="Submit" severity="info" variant="outlined" raised />
-    </template>
-  </Menubar>
+  <Splitter style="height: 900px" class="mb-8">
+    <SplitterPanel class="flex items-center justify-center" :size="15">
+      <div class="card flex justify-center">
+        <PanelMenu :model="panelItems" multiple class="w-full md:w-80" />
+      </div>
+    </SplitterPanel>
+    <SplitterPanel class="flex items-center justify-center" :size="85">
+      <Menubar :model="menubarItems">
+        <template #end>
+            <InputText id="game_search" v-model="text" placeholder="Search" />
+            <Button label="Submit" severity="info" variant="outlined" raised />
+        </template>
+      </Menubar>
+    </SplitterPanel>
+  </Splitter>
 </template>
 
 <style scoped>
 button, input {
   margin: 0 2px;
+}
+
+.p-splitterpanel {
+  padding: 20px 20px;
 }
 </style>
